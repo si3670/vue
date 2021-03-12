f9c99813c511b61bd32ea4c95d1812d5543aa7dc
si3670 <si3670@naver.com>
2021-03-12 22:58:15 +0900

    불필요한 프로그램 제거

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, eius ratione fugit distinctio nesciunt tempore quo iure illum, et in tenetur quisquam dicta! Deleniti, deserunt culpa iste distinctio natus possimus?
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'App',
+})
+</script>
+
+<style>
+html, body {
+  margin:0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
